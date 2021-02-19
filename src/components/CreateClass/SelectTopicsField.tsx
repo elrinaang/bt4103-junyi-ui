52d1928b823403d7c8e7f70597819350d929fe82
Elrina Ang <elrinaang1004@gmail.com>
2021-02-19 16:13:52 +0800

    added second and third steps of create class form

--- src/components/CreateClass/SelectTopicsField.tsx
+++ src/components/CreateClass/SelectTopicsField.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Input from '@material-ui/core/Input';
 import { createStyles, makeStyles, useTheme, Theme } from '@material-ui/core/styles';
@@ -13,7 +12,7 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     formControl: {
       margin: theme.spacing(1),
-      minWidth: 120,
+      minWidth: '90%',
     },
     chips: {
       display: 'flex',
@@ -27,17 +26,6 @@ const useStyles = makeStyles((theme: Theme) =>
     },
   }),
 );
-
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
 
 const topics = [
   'area',
@@ -53,7 +41,6 @@ const topics = [
 
 const SelectTopicsField: React.FC = () => {
   const classes = useStyles();
-  const theme = useTheme();
   const [personName, setPersonName] = React.useState<string[]>([]);
 
   const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
@@ -66,25 +53,21 @@ const SelectTopicsField: React.FC = () => {
         Select Topics
       </Typography>
       <FormControl className={classes.formControl}>
-      <Select
-        labelId="demo-mutiple-checkbox-label"
-        id="demo-mutiple-checkbox"
-        multiple
-        value={personName}
-        onChange={handleChange}
-        input={<Input />}
-        renderValue={(selected) => (selected as string[]).join(', ')}
-        MenuProps={MenuProps}
-        fullWidth
-      >
-        {topics.map((name) => (
-          <MenuItem key={name} value={name}>
-            <Checkbox checked={personName.indexOf(name) > -1} color="primary"/>
-            <ListItemText primary={name} />
-          </MenuItem>
-        ))}
-      </Select>
-    </FormControl>
+        <Select
+          multiple
+          value={personName}
+          onChange={handleChange}
+          input={<Input />}
+          renderValue={(selected) => (selected as string[]).join(', ')}
+        >
+          {topics.map((name) => (
+            <MenuItem key={name} value={name}>
+              <Checkbox checked={personName.indexOf(name) > -1} color="primary"/>
+              <ListItemText primary={name} />
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
     </React.Fragment>
   );
 };
--- src/components/CreateClass/UploadFileField.tsx
+++ src/components/CreateClass/UploadFileField.tsx
@@ -1,27 +1,70 @@
 import React from 'react';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 
 const UploadFileField: React.FC = () => {
+
+  const fileRef = React.useRef();
+  const [ file, setFile ] = React.useState(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    if (!event.target.files) return;    
+    setFile(event.target.files[0]);      
+    console.log(event.target.files[0])
+  };
+
+  const handleFileUpload = () => { 
+    console.log('file uploaded');
+  }  
 
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
         Upload File
       </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <TextField
-            required
-            id="address1"
-            name="address1"
-            label="Name of new class"
-            fullWidth
-            autoComplete="shipping address-line1"
-          />
+      <form 
+        onSubmit={handleFileUpload}
+        noValidate 
+        autoComplete="off" 
+      >
+        <Grid 
+          container 
+          spacing={1} 
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          <Grid item xs={3}>
+            <Input 
+              id="select-file"
+              ref={fileRef}
+              type="file" 
+              inputProps={{accept: ".csv"}}              
+              onChange={handleFileChange}      
+              style={{ display: "none" }}        
+            />
+            <TextField          
+              label="Selected File"                
+              value={file ? file.name : "No file selected"}
+              InputProps={{
+              readOnly: true,
+              }}
+            />            
+          </Grid>
+          <Grid item xs={3}>
+            <InputLabel htmlFor="select-file">
+              <Button variant="contained" component="span">
+                Select File
+              </Button>
+            </InputLabel>
+          </Grid>  
         </Grid>
-      </Grid>
+      </form>
     </React.Fragment>
   );
 };
